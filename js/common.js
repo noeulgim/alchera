baec4a102b51a43f39cace75b8c8eb7f2293e87f
noeulgim <noeul3240@gmail.com>
2022-10-24 01:15:06 +0900

    update

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-  var swiper = new Swiper(".mySwiper", {
+  var swiperThird = new Swiper(".swiper-third", {
         spaceBetween: 100,
         centeredSlides: true,
         slidesPerView: 'auto',
@@ -18,4 +18,43 @@ window.onload = function(){
           prevEl: ".swiper-button-prev",
         },
       });
+
+    var swiperFourth = new Swiper(".swiper-fourth", {
+      slidesPerView: 3,
+      direction: getDirection(),
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      on: {
+        resize: function () {
+          swiper.changeDirection(getDirection());
+        },
+      },
+    });
+
+    function getDirection() {
+      var windowWidth = window.innerWidth;
+      var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+      return direction;
+    }
+
+    // var swiperFifth = new Swiper("swiper-fifth", {
+    //   slidesPerView: 3,
+    //   grid: {
+    //     rows: 2,
+    //   },
+    //   autoplay: {
+    //     delay: 0,
+    //     disableOnInteraction: false,
+    //   },
+    //   spaceBetween: 120,
+    //   loop: true,
+    //   loopAdditionalSlides : 1,
+    //   pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    //   },
+    // });
 };
