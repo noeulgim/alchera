3e224fad99fa6e282fdc61d127373cc26fe1b560
noeulgim <noeul3240@gmail.com>
2022-10-23 22:23:04 +0900

    update

--- js/common.js
+++ js/common.js
@@ -1,82 +1,21 @@
 window.onload = function(){
-  var newsSwiper = new Swiper(".news_swiper", {
-		slidesPerView: 4,
-		spaceBetween: 40,
-		observer: true,
-		observeParents: true,
-		noSwiping: true,
-            roundLengths: true,
-		navigation: {
-			nextEl: '.news_swiper .swiper-next',
-			prevEl: '.news_swiper .swiper-prev',
-		},
-		pagination: {
-			el: '.swiper-pagination01',
-			type: 'bullets',
-			clickable: true,
-		},
-		autoHeight: false,
-		breakpoints: {
-			768: {
-				slidesPerView: 1,
-				spaceBetween: 30,
-				noSwiping: false,
-				followFinger: false
-			},
-			1440: {
-				slidesPerView: 4,
-				spaceBetween: 20,
-				noSwiping: false,
-
-			}
-		}
-	});
-
-  var industriesTabSwiper = new Swiper(".con_industries_tab", {
-        slidesPerView: 4,
-        observer: true,
-        observeParents: true,
-        noSwiping: true,
-        autoHeight: false,
-        breakpoints: {
-            768: {
-                slidesPerView: 'auto',
-                spaceBetween: 20,
-                noSwiping: false
-            }
-        }
-
-    });
-    var industriesSwiper = new Swiper(".con_industries_swiper", {
-        initialSlide: 0,
-        slidesPerView: 1.9,
-        centeredSlides: true,
+  var swiper = new Swiper(".mySwiper", {
         spaceBetween: 100,
-        autoplay: { delay: 3000 },
-        loop: true,
-        loopAdditionalSlides: 2,
-        noSwiping: false,
-        disableOnInteraction: true,
-        navigation: {
-            nextEl: '.swiper-next',
-            prevEl: '.swiper-prev',
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
         },
-        breakpoints: {
-            768: {
-                slidesPerView: 1,
-                spaceBetween: 20,
-                autoHeight: false
-            },
-            960: {
-                spaceBetween: 30,
-                slidesPerView: 1,
-
-            },
-            1440: {
-                slidesPerView: 1.9,
-                spaceBetween: 60,
-
-            }
-        }
-    });
+        loop: true,
+        loopAdditionalSlides : 1,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
 };
