6dde20cad8122f3f056acba8d0595c5e49bef99a
noeulgim <noeul3240@gmail.com>
2022-10-24 16:30:19 +0900

    update

--- js/common.js
+++ js/common.js
@@ -1,4 +1,14 @@
-window.onload = function(){
+$(function(){
+
+  $('#header-wrap').mouseenter(function(){
+    $('#logo h1 img').attr('src','./img/alchera_logo.svg');
+  }).mouseleave(function(){
+    $('#logo h1 img').attr('src','./img/alchera_logo_w.svg');
+  });
+
+
+//-------------------------------- swiper ----------------------------------
+
   var swiperThird = new Swiper(".swiper-third", {
         spaceBetween: 100,
         centeredSlides: true,
@@ -20,41 +30,34 @@ window.onload = function(){
       });
 
     var swiperFourth = new Swiper(".swiper-fourth", {
-      slidesPerView: 3,
-      direction: getDirection(),
+      slidesPerView: 4,
+      // direction: getDirection(),
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
-      on: {
-        resize: function () {
-          swiper.changeDirection(getDirection());
-        },
-      },
+      // slidesPerView: 4,
+      // spaceBetween: 30,
+      // pagination: {
+      //   el: ".swiper-pagination",
+      //   clickable: true,
+      // },
     });
 
-    function getDirection() {
-      var windowWidth = window.innerWidth;
-      var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
-
-      return direction;
-    }
-
-    // var swiperFifth = new Swiper("swiper-fifth", {
-    //   slidesPerView: 3,
-    //   grid: {
-    //     rows: 2,
-    //   },
-    //   autoplay: {
-    //     delay: 0,
-    //     disableOnInteraction: false,
-    //   },
-    //   spaceBetween: 120,
-    //   loop: true,
-    //   loopAdditionalSlides : 1,
-    //   pagination: {
-    //     el: ".swiper-pagination",
-    //     clickable: true,
-    //   },
-    // });
-};
+    var industriesSwiper = new Swiper(".swiper-fifth", {
+        initialSlide: 0,
+        slidesPerView: 'auto',
+        centeredSlides: true,
+        spaceBetween: 0,
+        autoplay: { delay: 0 },
+        speed: 5000,
+        loop: true,
+        loopAdditionalSlides: 1,
+        noSwiping: false,
+        disableOnInteraction: true,
+        navigation: {
+            nextEl: '.swiper-next',
+            prevEl: '.swiper-prev',
+        },
+      });
+});
